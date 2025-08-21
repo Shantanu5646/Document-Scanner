359d4ae33de3b21e0a4f10d4c54fe07a6d3f6db4
Shantanu5646 <118045616+Shantanu5646@users.noreply.github.com>
2025-08-20 21:27:04 -0400

    Add files via upload

---
+++ test_imports.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import scipy
+import matplotlib
+print(scipy.__version__)
+print(matplotlib.__version__)
+print("OpenCV version:", cv2.__version__)
+print("NumPy version:", np.__version__)
